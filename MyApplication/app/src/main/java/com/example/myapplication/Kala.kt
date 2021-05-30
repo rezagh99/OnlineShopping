42005e425947c8f141870e184b52bbf01eb83c4b
Mercedeh <Imercedeh@gmail.com>
2021-05-31 02:31:22 +0430

    Remove extra project!

--- MyApplication/app/src/main/java/com/example/myapplication/Kala.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-
-data class Kala(val name: String, val price: Double, val company: String, val type_of: String, val colour: String) {
-        // simple version
-        override fun toString(): String {
-            return name
-        }
-}
